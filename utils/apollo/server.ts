699a4a54bd835e87b98f29ef746fc789156f2af0
Tyler Lee <tyler7688@gmail.com>
2025-09-02 20:02:23 -0700

    URI for apollo client to conditional env

--- utils/apollo/server.ts
+++ utils/apollo/server.ts
@@ -10,11 +10,10 @@ export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
 	return new ApolloClient({
 		cache: new InMemoryCache(),
 		link: new HttpLink({
-			// uri:
-			// 	process.env.NODE_ENV === "development"
-			// 		? "http://localhost:8080/graphql/"
-			// 		: process.env.NEXT_PUBLIC_GRAPHQL_URL,
-			uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
+			uri:
+				process.env.NODE_ENV === "development"
+					? "http://localhost:8080/graphql/"
+					: process.env.NEXT_PUBLIC_GRAPHQL_URL,
 			fetchOptions: {},
 		}),
 	});

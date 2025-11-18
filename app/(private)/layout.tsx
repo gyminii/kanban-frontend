478e3e435887a883c1df4214e75919d7b9687326
Gyeong-Min Lee <tyler7688@gmail.com>
2025-11-18 13:23:38 +0900

    Demo

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,16 +1,13 @@
 import SidebarProvider from "@/components/sidebar/sidebar-provider";
-import { ApolloProvider } from "@/utils/apollo/provider";
 
 export default async function Layout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<ApolloProvider>
-			<SidebarProvider>
-				<div className="flex flex-1 min-h-0 flex-col bg-gradient-to-br from-slate-50 via-white to-indigo-50/30 dark:from-slate-950 dark:via-slate-900 dark:to-slate-900">
-					{children}
-				</div>
-			</SidebarProvider>
-		</ApolloProvider>
+		<SidebarProvider>
+			<div className="flex flex-1 min-h-0 flex-col bg-gradient-to-br from-slate-50 via-white to-indigo-50/30 dark:from-slate-950 dark:via-slate-900 dark:to-slate-900">
+				{children}
+			</div>
+		</SidebarProvider>
 	);
 }
